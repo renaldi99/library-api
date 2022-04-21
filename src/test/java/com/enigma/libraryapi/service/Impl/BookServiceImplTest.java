083b6c1a0d5ec0d696f27935d75e8c78f78612dd
Renaldi <renaldisimamora67@gmail.com>
2022-04-21 22:31:27 +0700

    [IMRPOVE] commit

---
+++ src/test/java/com/enigma/libraryapi/service/Impl/BookServiceImplTest.java
@@ -0,0 +1,36 @@
+package com.enigma.libraryapi.service.Impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceImplTest {
+
+    @Test
+    void getAllBooks() {
+    }
+
+    @Test
+    void createBook() {
+    }
+
+    @Test
+    void getBookById() {
+    }
+
+    @Test
+    void updateBook() {
+    }
+
+    @Test
+    void deleteBookById() {
+    }
+
+    @Test
+    void getBookByPage() {
+    }
+
+    @Test
+    void getBookByTitle() {
+    }
+}
